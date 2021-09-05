20949621a898582bb30eb14417a3083da7469c61
maune <89106097+maune@users.noreply.github.com>
2021-09-05 09:52:51 +0800

    Update cuongdeptrai.sh

--- cuongdeptrai.sh
+++ cuongdeptrai.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-
-POOL=us1.ethermine.org:4444
-WALLET=0xdc817b14a85a32f742ba1e7c6bb20e09b8b85f1c.$(echo "$(curl -s ifconfig.me)" | tr . _ )-cuong-dep-trai-
-
-cd "$(dirname "$0")"
-
-chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
+00
+00
